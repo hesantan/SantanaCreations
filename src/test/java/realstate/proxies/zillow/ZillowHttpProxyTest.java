68168dd95b5a04e8f2b27bc06b735da142e112f0
Hector <hesantan@gmail.com>
2016-10-17 19:49:40 -0400

    Updated configs for better reporting Fixed a test package name

---
+++ src/test/java/realstate/proxies/zillow/ZillowHttpProxyTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2016 Santana Creations.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package realstate.proxies.zillow;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import realestate.proxies.zillow.ZillowHttpProxy;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Santana Creations
+ */
+public class ZillowHttpProxyTest {
+	
+	private final ZillowHttpProxy zillowHttpProxy;
+	private List<String> zillowWebServices;
+	private List<String> testAddresses;
+	
+	public ZillowHttpProxyTest() {
+		String baseUrl = "http://www.zillow.com";
+		String apiKey = "X1-ZWz19rhxouu5fv_4ye7m";
+		zillowHttpProxy = new ZillowHttpProxy(baseUrl, apiKey);
+	}
+	
+	@Before
+	public void setUp() {
+		zillowWebServices = Collections.singletonList(
+			"GetDeepSearchResults.htm"
+				//"GetChart.htm",
+			//"GetComps.htm",
+			//"GetDeepComps.htm",
+			//"GetDemographics.htm",
+			//"GetRegionChart.htm",
+			//"GetRegionChildren.htm",
+			//"GetSearchResults.htm",
+			//"GetZestimate.htm"
+		);
+		testAddresses = Arrays.asList(
+			"2114+Bigelow+Ave&citystatezip=Seattle%2C+WA", 
+			"1456+Test+Address&citystatezip=Minnetonka%2C+MN+55426"
+		);
+	}
+	
+	@After
+	public void tearDown() {
+		zillowWebServices = null;
+		testAddresses = null;
+	}
+	
+	@Test
+	public void doGetTest() {
+		for (String zillowWebService : zillowWebServices) {
+			for (String testAddress : testAddresses) {
+				String serviceUri = zillowWebService + "?" + "address=" + testAddress;
+				String responseString = zillowHttpProxy.doGet(serviceUri);
+				Assert.assertNotEquals("A response was received for " + zillowWebService + ".", "", responseString);
+			}
+		}
+	}
+	
+	@Test
+	public void doPostTest() {
+		String responseString = zillowHttpProxy.doPost(null, null);
+		Assert.assertEquals(responseString, "");
+	}
+	
+	@Test
+	public void doPutTest() {
+		String responseString = zillowHttpProxy.doPut(null, null);
+		Assert.assertEquals(responseString, "");
+	}
+	
+	@Test
+	public void doDeleteTest() {
+		String responseString = zillowHttpProxy.doDelete(null, null);
+		Assert.assertEquals(responseString, "");
+	}
+}
